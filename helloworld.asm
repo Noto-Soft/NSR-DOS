accee4131845d54c58f78e406dcc9f322c8dfbe5
bismuth <70106649+bismuthnickel@users.noreply.github.com>
2025-06-24 20:06:34 -0400

    add symbol table to helloworld.exe

--- helloworld.asm
+++ helloworld.asm
@@ -8,6 +8,8 @@ db "AD"
 db 2
 db 0
 dw start
+dw symbol_table
+dw SYMBOL_TABLE_LENGTH
 
 msg: db "Hello world!", endl, 0
 
@@ -19,5 +21,10 @@ start:
     lea si, [msg]
     int 0x21
     retf
+
+symbol_table:
+db "start", 0
+dw start
+SYMBOL_TABLE_LENGTH equ 4
 
 times 512-($-$$) db 0
