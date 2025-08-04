20030915ded2753696ce4ed1e3e100a11c5bba47
bismuthnickel <warreneverettstevens@gmail.com>
2025-08-04 13:39:23 -0400

    buildshing

--- build.sh
+++ build.sh
@@ -1,36 +1,41 @@
+add_to_disk() {
+  	disk_image="$1"
+  	shift
+  	for file in "$@"; do
+    	python3 tools/thinfs.py add "$disk_image" "$file"
+  	done
+}
+
 mkdir -p build
 
 nasm src/boot.asm -f bin -o build/boot.bin
-nasm src/kernel.asm -f bin -o build/kernel.bin
+nasm src/kernel.asm -f bin -o build/kernel.sys
 nasm src/command.asm -f bin -o build/command.exe
 nasm src/helloworld.asm -f bin -o build/helloworld.exe
 nasm src/graphix.asm -f bin -o build/graphix.exe
 
 python3 tools/thinfs.py createbootable nsr-dos.img build/boot.bin NSRDOS
-python3 tools/thinfs.py add nsr-dos.img build/kernel.bin KERNEL.SYS
-python3 tools/thinfs.py add nsr-dos.img assets/text/boot.txt
-python3 tools/thinfs.py add nsr-dos.img build/command.exe
-python3 tools/thinfs.py add nsr-dos.img build/helloworld.exe
-python3 tools/thinfs.py add nsr-dos.img assets/text/aldi.txt
-python3 tools/thinfs.py add nsr-dos.img build/graphix.exe
-python3 tools/thinfs.py add nsr-dos.img assets/images/nsrdos.bmp
-python3 tools/thinfs.py add nsr-dos.img assets/images/wisconsin.bmp
-python3 tools/thinfs.py add nsr-dos.img assets/images/meme.bmp
-python3 tools/thinfs.py add nsr-dos.img build/heaptest.exe
+add_to_disk nsr-dos.img \
+  	build/kernel.sys \
+  	assets/text/boot.txt \
+  	build/command.exe \
+  	build/helloworld.exe \
+  	assets/text/aldi.txt \
+  	build/graphix.exe \
+  	assets/images/nsrdos.bmp \
+  	assets/images/wisconsin.bmp \
+  	assets/images/meme.bmp
 truncate -s 1440k nsr-dos.img
 
 nasm src/basic.asm -f bin -o build/basic.exe
 nasm src/heaptest.asm -f bin -o build/heaptest.exe -w-zeroing
 python3 tools/thinfs.py create disk-2.img BDRIVE
-python3 tools/thinfs.py add disk-2.img assets/text/bdrive.txt
-python3 tools/thinfs.py add disk-2.img build/basic.exe
-python3 tools/thinfs.py add disk-2.img build/heaptest.exe
-python3 tools/thinfs.py add disk-2.img assets/text/gg.txt
-for FILE in docs/*; do
-	if [ -f "$FILE" ]; then
-		python3 tools/thinfs.py add disk-2.img $FILE
-	fi
-done
+add_to_disk disk-2.img \
+	assets/text/bdrive.txt \
+	build/basic.exe \
+	build/heaptest.exe \
+	assets/text/gg.txt \
+	$(find docs/ -maxdepth 1 -type f -print)
 truncate -s 1440k disk-2.img
 
 rm -rf build/
